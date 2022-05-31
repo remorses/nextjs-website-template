fab394152d1be5fad096000308cb610f657dc136
remorses <beats.by.morse@gmail.com>
2022-05-31 14:54:33 +0200

    specific

--- website/src/components/specific.tsx
+++ website/src/components/specific.tsx
@@ -1,11 +1,26 @@
+import NextLink from 'next/link'
 import { Header } from 'beskar/src/Header'
-import { Footer, Link } from 'beskar/landing'
+import { Footer, Link, useColorMode, useColorModeValue } from 'beskar/landing'
+import {
+    MoonIcon,
+    SunIcon,
+    CogIcon,
+    LogoutIcon,
+    CreditCardIcon,
+} from '@heroicons/react/outline'
+import { Avatar } from '@nextui-org/react'
+import { DropDownMenu } from 'beskar/src/DropDown'
+import { signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { AvatarButton } from 'beskar/src/Header'
 
 export function MyHeader({}) {
     return (
         <Header
+            logo={<Logo />}
             links={[
                 <Link href='/docs'>Docs</Link>,
+                <AvatarMenu />,
                 //
             ]}
         />
@@ -23,5 +38,69 @@ export function MyFooter({}) {
                 ],
             }}
         />
+    )
+}
+
+function AvatarMenu({ imgSrc = '' }) {
+    const { toggleColorMode, isDark } = useColorMode()
+    const router = useRouter()
+    const orgId = router.query.orgId
+    let avatar = (
+        <div className=''>
+            <AvatarButton
+                // squared
+                // textColor={'white'}
+                // color='gradient'
+                className={'border'}
+                name={'hello'}
+            />
+        </div>
+    )
+
+    return (
+        <DropDownMenu button={avatar}>
+            <DropDownMenu.Item
+                onClick={toggleColorMode}
+                icon={useColorModeValue(
+                    <MoonIcon className='w-5 h-5 opacity-60' />,
+                    <SunIcon className='w-5 h-5 opacity-60' />,
+                )}
+            >
+                {!isDark ? 'Dark mode' : 'Light Mode'}
+            </DropDownMenu.Item>
+            <NextLink href={`/org/${orgId}/settings`}>
+                <DropDownMenu.Item
+                    icon={<CogIcon className='w-5 h-5 opacity-60' />}
+                >
+                    Settings
+                </DropDownMenu.Item>
+            </NextLink>
+            <DropDownMenu.Item
+                onClick={() => {
+                    // updateUpgradeModalState({
+                    //     isOpen: true,
+                    //     reason: '',
+                    // })
+                }}
+                icon={<CreditCardIcon className='w-5 h-5 opacity-60' />}
+            >
+                Upgrade
+            </DropDownMenu.Item>
+            <DropDownMenu.Item
+                onClick={() => signOut({ callbackUrl: '/' })}
+                icon={<LogoutIcon className='w-5 h-5 opacity-60' />}
+            >
+                Sign out
+            </DropDownMenu.Item>
+        </DropDownMenu>
+    )
+}
+
+export function Logo({}) {
+    const { status } = useSession()
+    return (
+        <NextLink href={status === 'authenticated' ? '/org' : '/'} passHref>
+            <a className='text-xl font-semibold '>Replicant</a>
+        </NextLink>
     )
 }
