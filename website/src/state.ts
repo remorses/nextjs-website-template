e910ca720033745881ae2fd4273cb19b73081890
remorses <beats.by.morse@gmail.com>
2022-06-04 13:30:10 +0200

    website/src: cleaning

--- website/src/state.ts
+++ website/src/state.ts
@@ -1,6 +1,1 @@
-import { Org } from 'prisma-client'
-import { proxy, subscribe } from 'valtio'
-
-const isBrowser = typeof window !== 'undefined'
-
-export const currentlySelectedOrg = proxy<Org | undefined>()
+export {}
