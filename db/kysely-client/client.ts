0d62e15e1b5c714b817ef715017a80fc0c92713c
remorses <beats.by.morse@gmail.com>
2022-07-03 16:52:19 +0200

    db/kysely-client/client:

--- db/kysely-client/client.ts
+++ db/kysely-client/client.ts
@@ -6,7 +6,7 @@ import * as types from './generated'
 
 export * from './generated'
 
-interface Database {
+export interface DatabaseTables {
    Account: types.SqlAccount,
     Campaign: types.SqlCampaign,
     Org: types.SqlOrg,
@@ -19,43 +19,17 @@ interface Database {
     VerificationToken: types.SqlVerificationToken
 }
 
-// only 1 connection at a time because initial connection is slow af
 const pool = createPool({
     enableKeepAlive: true,
     connectionLimit: 30,
     waitForConnections: true,
     // ssl: { rejectUnauthorized: true },
-    
+    uri: process.env.DATABASE_URL,
 })
 
-export const db = new Kysely<Database>({
+export const db = new Kysely<DatabaseTables>({
     dialect: new MysqlDialect({
         pool: pool,
     }),
 })
-
-function parseUrl(url) {
-    const parsedUrl = new URL(url)
-    const options: any = {
-        host: parsedUrl.hostname,
-        port: parsedUrl.port,
-        database: parsedUrl.pathname.substr(1),
-        user: parsedUrl.username,
-        password: parsedUrl.password,
-    }
-    parsedUrl.searchParams.forEach((value, key) => {
-        // this is passed with the prisma url, ignore it
-        if (key === 'sslaccept') {
-            return
-        }
-        try {
-            // Try to parse this as a JSON expression first
-            options[key] = JSON.parse(value)
-        } catch (err) {
-            // Otherwise assume it is a plain string
-            options[key] = value
-        }
-    })
-    return options
-}
 
