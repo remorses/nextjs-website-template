638954b9b8870576cbddd9e59784917086c6c1ea
remorses <beats.by.morse@gmail.com>
2022-07-14 18:18:59 +0200

    website/src/utils:

--- website/src/utils/index.ts
+++ website/src/utils/index.ts
@@ -1,17 +1,1 @@
-import { useTheme } from 'next-themes'
-import { useState, useEffect } from 'react'
-import toast from 'react-hot-toast'
-
-
-export class KnownError extends Error {
-    constructor(message: string) {
-        super(message)
-        this.name = 'KnownError'
-    }
-}
-export class AppError extends Error {
-    constructor(message: string) {
-        super(message)
-        this.name = 'AppError'
-    }
-}
+export * from './utils'
