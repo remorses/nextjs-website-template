ff05ca6af8853b5976fd7092f46ba912bac2ea6b
remorses <beats.by.morse@gmail.com>
2022-07-18 19:38:51 +0200

    cleaning

---
+++ README.md
@@ -0,0 +1,4 @@
+Creating a new project from template
+
+-   replace port with new one (makes it easier to work on many projects at once)
+-   replace `7050` with PORT
--- website/e2e/playwright.config.ts
+++ website/e2e/playwright.config.ts
@@ -1,27 +1,18 @@
 import { PlaywrightTestConfig } from '@playwright/test'
 
-export const testData = {
-    ROOT_URL: 'http://localhost:7050',
-    NAME: 'MY NAME',
-
-    EMAIL: 'myemail@gmail.com',
-    storageState: 'storageState.json',
-}
-
 export function sleep(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 const config: PlaywrightTestConfig = {
     globalSetup: require.resolve('./global-setup'),
-    
     outputDir: 'output',
     use: {
         browserName: 'chromium',
         headless: !Boolean(process.env.HEADED),
 
         screenshot: 'only-on-failure',
-        baseURL: testData.ROOT_URL,
+        baseURL: 'http://localhost:7050',
         launchOptions: {
             // slowMo: 50,
         },
